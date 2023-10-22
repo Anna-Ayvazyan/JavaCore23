80ef3b355ceeee7fbce326fb4158c4ec3be62b12
anna <anna.ayvazyan.exp@gmail.com>
2023-10-22 22:21:34 +0400

    First project

--- src/homework/Example.java
+++ src/homework/Example.java
@@ -1,7 +1,18 @@
 package homework;
 
+import java.sql.SQLOutput;
+
 public class Example {
     int height;
     int width;
     int depth;
+    public int vol(){
+        return height * width * depth;
+    }
+
+    void set(int h, int w, int d){
+        height=h;
+        width=w;
+        depth=d;
+    }
 }
--- src/homework/Gener.java
+++ src/homework/Gener.java
@@ -3,12 +3,17 @@ package homework;
 public class Gener {
     public static void main(String[] args) {
         Example myexam = new Example();
-        int vol;
-        myexam.height = 100;
-        myexam.width = 2;
-        myexam.depth = 5;
-        vol = myexam.height * myexam.depth * myexam.width;
-        System.out.println("Res is " + vol);
+        Example myexam2 = new Example();
+
+
+
+        myexam.set(2, 10, 5);
+        myexam2.set(3, 1, 4);
+        myexam.vol();
+        myexam2.vol();
+
+        System.out.println(myexam.vol());
+        System.out.println(myexam2.vol());
 
     }
 }
